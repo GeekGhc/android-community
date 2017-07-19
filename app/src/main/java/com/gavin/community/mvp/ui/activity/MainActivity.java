ec285e80423da804b84fd5bc17ccb0e9406029ff
GeekGhc <ghcgavin@sina.com>
2017-07-19 08:28:27 +0800

    fix 版本问题

--- app/src/main/java/com/gavin/community/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/gavin/community/mvp/ui/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.gavin.community.mvp.ui.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -68,6 +72,17 @@ public class MainActivity extends SimpleActivity {
 
     @Override
     protected void initEventAndData() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);//改变状态栏颜色（可以和应用的标题颜色一样）
+
+        }
         prepareView();
         initData();
         initView();
